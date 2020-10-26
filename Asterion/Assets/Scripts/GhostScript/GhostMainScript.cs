0da08299cc6da5e5df62aa8eb44f8f8477ea1dfe
erriveros <erriveros@miuandes.cl>
2020-10-26 06:00:48 -0300

    camera now follows player, added text for player level

---
+++ Asterion/Assets/Scripts/GhostScript/GhostMainScript.cs
@@ -0,0 +1,16 @@
+﻿﻿using UnityEngine;
+
+namespace GhostScript
+{
+    public class GhostMainScript : MonoBehaviour
+    {
+        [SerializeField] internal GhostMovementScript movementScript;
+        [SerializeField] internal GhostStateScript stateScript;
+
+        private void Awake()
+        {
+           
+            
+        }
+    }
+}

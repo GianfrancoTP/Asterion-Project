49e6309d1aee1242e039438c1ceec603e24895f7
Emilio Arias <earias_1995@hotmail.com>
2020-10-26 17:24:47 -0300

    Minotaur sprite

---
+++ Assets/MinotaurAgro.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class MinotaurAgro : MonoBehaviour
+{
+    [SerializeField]
+    Transform player;
+
+    [SerializeField]
+    float aggroRange;
+
+    [SerializeField]
+    float moveSpeed;
+
+    Rigidbody2D rb2d;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //distance to player
+        float distanceToPlayer = Vector2.Distance(transform.position, player.position);
+
+        if (distanceToPlayer < aggroRange)
+        {
+            //code to chase player
+
+        }
+        else
+        { 
+            //stop chasing player
+        }
+
+    }
+}

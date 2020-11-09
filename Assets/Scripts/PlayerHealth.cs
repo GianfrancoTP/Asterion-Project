788ab4be0368b6ca00094d0f67e9e877f63d33ca
Emilio Arias <earias_1995@hotmail.com>
2020-11-09 19:29:36 -0300

    Agregue sprite de la cuerda

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float health;
+    public Slider slider;
+
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        slider.value = health;
+
+    }
+
+    void OnCollisionEnter(Collision enemigo)
+    {
+        if (enemigo.gameObject.tag == "Minotaur") 
+        {
+            health = health - 50f;
+            Debug.Log("Minotauro choco");
+        }
+
+        
+    }
+}

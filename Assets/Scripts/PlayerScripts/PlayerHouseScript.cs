f2aa74fe5b5f33435ccba7129cd16d74eb5cf9fc
GianfrancoTP <you@example.com>
2020-11-10 12:25:15 -0300

    sprite change option

--- Assets/Scripts/PlayerScripts/PlayerHouseScript.cs
+++ Assets/Scripts/PlayerScripts/PlayerHouseScript.cs
@@ -10,6 +10,9 @@ namespace PlayerScripts
         public Text houseNameText;
         [SerializeField] private PlayerMainScript mainScript;
         internal int armor = 1;
+        public GameObject ares;
+        public Sprite hermes;
+        public Sprite zeus;
 
         void SetHouse()
         {
@@ -20,10 +23,22 @@ namespace PlayerScripts
         private void Start()
         {
             house = MainMenu.houseName;
-            //house = "ARES";
+            if (house == "HERMES") 
+            {
+                Replace(ares, hermes);
+            } else if (house == "ZEUS") 
+            {
+                Replace(ares, zeus);
+            }
             SetHouse();
         }
 
-     
+        void Replace(GameObject obj1, Sprite obj2)
+        {
+            //Instantiate(obj2, obj1.transform.position, Quaternion.identity);
+            //obj2.GetComponent<SpriteRenderer>().sortingOrder = 2;
+            obj1.GetComponent<SpriteRenderer>().sprite = obj2;
+            //Destroy(obj1);
+        }
     }
 }

1927576577a0c8401124fc7efd4cbfbcc5784d53
GianfrancoTP <you@example.com>
2020-12-07 22:11:33 -0300

    animations

--- Assets/Scripts/PlayerScripts/PlayerHouseScript.cs
+++ Assets/Scripts/PlayerScripts/PlayerHouseScript.cs
@@ -10,20 +10,39 @@ namespace PlayerScripts
         public Text houseNameText;
         [SerializeField] private PlayerMainScript mainScript;
         internal int armor = 1;
+        public GameObject skin;
+        public Sprite hermes;
+        public Sprite zeus;
+        public Sprite ares;
 
         void SetHouse()
         {
-            
             houseNameText.text = house;
+            if (house == "HERMES")
+            {
+                //setSkin(hermes);
+            }
+            else if (house == "ZEUS")
+            {
+                //setSkin(zeus);
+            } 
+            else if (house == "ARES")
+            {
+                //setSkin(ares);
+            }
         }
 
-        private void Start()
+        private void Update()
         {
             house = MainMenu.houseName;
-            //house = "ARES";
             SetHouse();
         }
 
-     
+        void setSkin(Sprite obj2)
+        {
+            Debug.Log("cambiando sprite");
+            skin.GetComponent<SpriteRenderer>().sprite = obj2;
+            Debug.Log("cambiando sprite2");
+        }
     }
 }
